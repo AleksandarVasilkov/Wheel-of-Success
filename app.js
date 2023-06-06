731f8bbf89c16930cea228632aab92e5843f8957
Aleksandar Vasilkov <110114680+AleksandarVasilkov@users.noreply.github.com>
2023-06-06 23:19:42 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const phrases = [
   function addPhraseToDisplay(arr) {
     const phraseList = document.querySelector('#phrase ul');
   
-    // Clear any existing content
+    
     phraseList.innerHTML = '';
   
     for (let i = 0; i < arr.length; i++) {
